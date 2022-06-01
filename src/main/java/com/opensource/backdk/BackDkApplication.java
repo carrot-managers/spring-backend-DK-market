7a82491cf61b8009838f1171944553995e4a7db5
SMJin <sstaw@naver.com>
2022-06-01 16:12:44 +0900

    environment setting

---
+++ src/main/java/com/opensource/backdk/BackDkApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.backdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackDkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/opensource/backdk/BackDkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.backdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
